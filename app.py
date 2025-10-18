64b654a63eef2e93eac7d8774335f6b54b057356
guptagirdharilal <guptagirdharilal772@gmail.com>
2025-10-18 05:57:02 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from pyfiglet import Figlet
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    ascii_art = None
+    if request.method == 'POST':
+        text = request.form.get('text')
+        font = request.form.get('font')
+        if text and font:
+            figlet = Figlet(font=font)
+            ascii_art = figlet.renderText(text)
+    return render_template('index.html', ascii_art=ascii_art)
+
+if __name__ == '__main__':
+    app.run(debug=True)
